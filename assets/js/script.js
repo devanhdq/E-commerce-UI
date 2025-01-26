d8edd050cbdeb6995390179de07fed1a8b2a831d
AnhQuang <anhquang@AnhQuangs-MacBook-Pro.local>
2025-01-26 15:26:04 +0700

    init project and create navbar, header, featured components

---
+++ assets/js/script.js
@@ -0,0 +1,66 @@
+// Copy Menu
+function copyMenu() {
+  // copy inside .dpt-cat to .departments
+  let dptCategory = document.querySelector(".dpt-cat");
+  let dptPlace = document.querySelector(".departments");
+  dptPlace.innerHTML = dptCategory.innerHTML;
+
+  //   copy inside nav to nav
+  let mainNav = document.querySelector(".header-nav nav");
+  let navPlace = document.querySelector(".off-canvas nav");
+  navPlace.innerHTML = mainNav.innerHTML;
+
+  //   copy .header-top .wrapper to .the-top-nav
+  let topNav = document.querySelector(".header-top .wrapper");
+  let topPlace = document.querySelector(".off-canvas .the-top-nav");
+  topPlace.innerHTML = topNav.innerHTML;
+}
+copyMenu();
+
+// Show mobile menu
+const menuButton = document.querySelector(".trigger"),
+  closeButton = document.querySelector(".t-close"),
+  addClass = document.querySelector(".site");
+
+menuButton.addEventListener("click", function () {
+  addClass.classList.toggle("show-menu");
+});
+
+closeButton.addEventListener("click", function () {
+  addClass.classList.remove("show-menu");
+});
+
+// Show sub menu on mobile
+// const subMenu = document.querySelectorAll(".has-child .icon-small");
+// subMenu.forEach((menu) => menu.addEventListener("click", toggle));
+
+// function toggle(e) {
+//   e.preventDefault();
+//   subMenu.forEach((item) =>
+//     item != this ? item.closest(".has-child").classList.remove("expand") : null
+//   );
+//   if (this.closest(".has-child").classList != "expand");
+//   this.closest(".has-child").classList.toggle("expand");
+// }
+
+const subMenu = document.querySelectorAll(".has-child .icon-small");
+
+subMenu.forEach((menu) => menu.addEventListener("click", toggle));
+
+function toggle(e) {
+  e.preventDefault();
+  subMenu.forEach((item) => {
+    if (item !== this) {
+      item.closest(".has-child").classList.remove("expand");
+    }
+  });
+  this.closest(".has-child").classList.toggle("expand");
+}
+
+// Slider
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
